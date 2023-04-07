a280cef22cbf3b1dfcb93a73a2a9b0486f1330e2
Vitalii Bondarchuk <vitaliibondarchuk@truckstop.com>
2023-04-07 13:47:42 +0300

    add endpoint logging

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace WebApplication1.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Get Forecast");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
